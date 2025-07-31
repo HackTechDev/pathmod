93c7d36206dd6a62e283bac0750835d42a1514d6
Nekrofage <lesanglierdesardennes@gmail.com>
2025-07-31 19:34:58 +0200

    [Version] 0.0.1

---
+++ init.lua
@@ -0,0 +1,120 @@
+-- init.lua
+local storage = minetest.get_mod_storage()
+local paths = minetest.deserialize(storage:get_string("paths")) or {}
+local following_players = {}
+
+-- Sauvegarde des chemins
+local function save_paths()
+    storage:set_string("paths", minetest.serialize(paths))
+end
+
+-- Créer un nouveau chemin
+minetest.register_chatcommand("pathnew", {
+    params = "<nom>",
+    description = "Crée un nouveau chemin vide",
+    func = function(name, param)
+        if param == "" then
+            return false, "Usage: /pathnew <nom>"
+        end
+        paths[param] = {}
+        save_paths()
+        return true, "Chemin '" .. param .. "' créé."
+    end
+})
+
+-- Ajouter un point à un chemin
+minetest.register_chatcommand("pathadd", {
+    params = "<nom>",
+    description = "Ajoute la position actuelle au chemin nommé",
+    func = function(name, param)
+        if paths[param] == nil then
+            return false, "Ce chemin n'existe pas."
+        end
+        local player = minetest.get_player_by_name(name)
+        if player then
+            local pos = vector.round(player:get_pos())
+            table.insert(paths[param], pos)
+            save_paths()
+            return true, "Point ajouté au chemin '" .. param .. "'."
+        end
+    end
+})
+
+-- Lister les chemins existants
+minetest.register_chatcommand("pathlist", {
+    description = "Liste tous les chemins",
+    func = function(name)
+        local list = {}
+        for k, v in pairs(paths) do
+            table.insert(list, k .. " (" .. #v .. " points)")
+        end
+        if #list == 0 then
+            return true, "Aucun chemin défini."
+        end
+        return true, "Chemins : " .. table.concat(list, ", ")
+    end
+})
+
+-- Suivre un chemin
+minetest.register_chatcommand("pathfollow", {
+    params = "<nom>",
+    description = "Suis le chemin nommé",
+    func = function(name, param)
+        if paths[param] == nil or #paths[param] == 0 then
+            return false, "Ce chemin est vide ou inexistant."
+        end
+        following_players[name] = {path = param, index = 1}
+        return true, "Début du suivi du chemin '" .. param .. "'."
+    end
+})
+
+-- Supprimer un chemin
+minetest.register_chatcommand("pathclear", {
+    params = "<nom>",
+    description = "Supprime un chemin",
+    func = function(name, param)
+        if paths[param] then
+            paths[param] = nil
+            save_paths()
+            return true, "Chemin '" .. param .. "' supprimé."
+        end
+        return false, "Ce chemin n'existe pas."
+    end
+})
+
+-- Déplacement fluide du joueur
+minetest.register_globalstep(function(dtime)
+    for name, state in pairs(following_players) do
+        local player = minetest.get_player_by_name(name)
+        if player then
+            local path_points = paths[state.path]
+            local target = path_points[state.index]
+            if target then
+                local pos = player:get_pos()
+                local dir = vector.direction(pos, target)
+                local dist = vector.distance(pos, target)
+
+                -- Rotation du joueur
+                local yaw = math.atan2(dir.z, dir.x) - math.pi / 2
+                player:set_look_horizontal(yaw)
+
+                -- Avancer
+                local speed = 4 -- m/s
+                if dist > 0.3 then
+                    pos.x = pos.x + dir.x * dtime * speed
+                    pos.y = pos.y + dir.y * dtime * speed
+                    pos.z = pos.z + dir.z * dtime * speed
+                    player:set_pos(pos)
+                else
+                    -- Prochain point
+                    state.index = state.index + 1
+                    if state.index > #path_points then
+                        following_players[name] = nil
+                        minetest.chat_send_player(name, "Chemin terminé.")
+                    end
+                end
+            end
+        end
+    end
+end)
+
